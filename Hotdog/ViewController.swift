40582077ca20ebabda0745ca740064ca7d73e313
elina-mns <ellie.mansurova@gmail.com>
2020-12-28 18:47:34 -0500

    Initial Commit

---
+++ Hotdog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotdog
+//
+//  Created by Elina Mansurova on 2020-12-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
