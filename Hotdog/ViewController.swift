2496c62eb19b66318964a80f35b681d05627d2c2
elina-mns <ellie.mansurova@gmail.com>
2020-12-28 19:46:34 -0500

    get image recognition results back to our model

--- Hotdog/ViewController.swift
+++ Hotdog/ViewController.swift
@@ -25,8 +25,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = pickedImage
+            guard let ciImage = CIImage(image: pickedImage) else { fatalError("Could not convert UIImage to CIImage") }
+            
+            detect(image: ciImage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { fatalError("Loading CoreML model failed") }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else { fatalError("Could not convert results") }
+            print(results)
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
