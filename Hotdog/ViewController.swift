f6143257df9fb4197bf7663aff1ae027dd1b9948
elina-mns <ellie.mansurova@gmail.com>
2020-12-28 19:27:35 -0500

    request for a photo library, use of func didFinishPickingMediaWithInfo

--- Hotdog/ViewController.swift
+++ Hotdog/ViewController.swift
@@ -18,11 +18,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = pickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
-        
+        present(imagePicker, animated: true, completion: nil)
     }
     
 }
